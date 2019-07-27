5452a20f5de7025ba9bdd40e851afde7fc67c5a5
Gary Namestnik <gazzenger@gmail.com>
2019-07-27 11:19:46 +0800

    added example for compressing archive

--- bulk-zip-unzip.ps1
+++ bulk-zip-unzip.ps1
@@ -1,13 +1,17 @@
 $dirPath = "C:\Users\gnamestnik\Desktop\archives"
 $destPath = "C:\Users\gnamestnik\Desktop\unzipped"
+$filter = "*.zip"
 
 $folders = get-childitem -path $dirPath | where-object {$_.Psiscontainer}
 $i=0
 foreach ($d in $folders){
     echo "$i of $($folders.Count)"
-    $files = Get-ChildItem $d.FullName -Filter *.zip
+    $files = Get-ChildItem $d.FullName -Filter $filter
     foreach ($f in $files){
+        #expanding archive
         Expand-Archive -LiteralPath $f.FullName -DestinationPath $destPath
+        #comptressing archive
+        #Compress-Archive -LiteralPath 'C:\Reference\Draft Doc.docx', 'C:\Reference\Images\Diagram [2].vsd'  -CompressionLevel Optimal -DestinationPath C:\Archives\Draft.Zip
         $alarmFile = $destPath+"\Alarms.dat"
         $newname   = $f.BaseName+"-Alarms.dat"
         if (Test-Path $alarmFile) {
